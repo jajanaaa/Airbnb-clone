2d0d4e452abc48613c165a9c3f122fffa3e58447
janamanzana <96806525+janamanzana@users.noreply.github.com>
2022-08-09 22:38:12 +0200

    finished icons bar

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 const leftButton = document.querySelector(".left");
 const rightButton = document.querySelector(".right");
+const container = document.querySelector(".icons-container");
 
-function rightScroll() {
-  const right = document.querySelector(".scroll-images");
-  right.scrollBy(200, 0);
-  leftButton.style.opacity = "1";
-}
+leftButton.addEventListener("click", function () {
+  container.scrollBy(-200, 0);
+});
 
-function leftScroll() {
-  const left = document.querySelector(".scroll-images");
-  left.scrollBy(-200, 0);
-  //   rightButton.style.opacity = "0";
-}
+rightButton.addEventListener("click", function () {
+  container.scrollBy(200, 0);
+});
